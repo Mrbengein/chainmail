23afbd0514f36a82b7313cb8fa69f1b1ffed55d8
Bengein <63845399+Mrbengein@users.noreply.github.com>
2026-02-16 15:23:55 +0100

    Create server.js

---
+++ gateway/server.js
@@ -0,0 +1,36 @@
+version: "3.9"
+
+services:
+
+  besu:
+    image: hyperledger/besu:latest
+    command: >
+      --network=dev
+      --rpc-http-enabled
+      --rpc-http-api=ETH,NET,WEB3
+    ports:
+      - "8545:8545"
+
+  registry:
+    build: ./registry-service
+    environment:
+      RPC_URL: http://besu:8545
+    depends_on:
+      - besu
+
+  gateway:
+    build: ./gateway
+    ports:
+      - "2525:2525"
+    depends_on:
+      - registry
+
+  db:
+    image: postgres:15
+    environment:
+      POSTGRES_PASSWORD: w3mail
+
+  message:
+    build: ./message-service
+    depends_on:
+      - db
